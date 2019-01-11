c9ee46a9eb5a305ceab869ed260349024c09fd31
surencool4 <surencool4@gmail.com>
2019-01-11 12:36:44 +0545

    Initial commit

---
+++ dist/inc/about.php
@@ -0,0 +1,39 @@
+<!--=========== Start About Section ============--> 
+<section id="se-2" class="about background-2 "> 
+<div class="container"> 
+	<div class="head-section">
+		<h2>About Me</h2>
+	</div>
+	 <div class="row"> 
+	 	<div class="col-lg-7 col-md-12">
+	 	 <div class="head-about mb-4"> 				 	 	
+	 	 	<p>For the past 6 years, I've been developing applications for the web using mostly <strong>PHP</strong>. I do this for a living and love what I do as every day there is something new and exciting to learn. <br>
+
+ The <b>Web Development</b> community is a big part of my life. Whether teaching code to student at a institute, managing online programming groups and blogs or attending a conference, I find keeping involved helps me stay up to date.<br>
+
+
+My job involves doing what I love, Developing new websites/applications, <b>customer relationship management (CRM)</b> applications and developing fantastic <strong>content management systems (CMS)</strong>.<br>
+
+I spend a lot of time learning new techniques and actively help other people learn <b>Web Development</b> through a variety of help groups and writing <b>Web Development</b> tutorials for my website and blog about advancements in <b>web design</b> and <b>development</b>.</p>
+	 	 </div> 
+	 	 <ul class="list-unstyled p-0"> 
+	 	 	<li><p>Name<span>:</span></p> <span>Surendra Budhathoki</span></li> 
+	 	 	<li><p>Age<span>:</span></p> <span>29 Years</span></li> 
+	 	 	<li><p>Phone<span>:</span></p> <span>+977 9803 729959</span></li> 
+	 	 	<li><p>E-mail<span>:</span></p> <span>surencool4@gmail.com</span></li> 
+	 	 	<li><p>Address<span>:</span></p> <span>TIA Airport, kathmandu, Nepal</span></li> 
+	 	 </ul> 
+	 	 <span class="bnt-about d-inline-block"><a class="text-uppercase" href="cv.pdf">Download CV</a></span> 
+	 	 <span class="bnt-about d-inline-block"><a class="text-uppercase" href="cover-letter.pdf">Download Cover Letter</a></span> 
+	 	</div> 
+	 	<div class="col-md-5 hidden-md"> 
+	 		<div class="img-about"> 
+	 			<a href="images/ac.jpg" data-lightbox="me" data-title="Surendra Budhathoki">
+	 				<img class="img-fluid" src="images/ac.jpg" alt="Suren image"></a> 
+	 		</div> 
+	 	</div> 
+	 </div> 
+	 <!-- row --> 
+	</div> 
+</section> 
+<!--=========== End About Section ============--> 
